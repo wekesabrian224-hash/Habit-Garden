0bdcfe39698e55838d330f0df1e351993e8ae2ff
victoraseno-coder <victor.aseno@student.moringaschool.com>
2026-08-17 08:02:48 +0300

    final commit

--- Habit-Garden/src/main.jsx
+++ Habit-Garden/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import BottomNav from "./components/victor/BottomNav";
+import Navbar from "./components/victor/Navbar";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BottomNav />
+    <Navbar />
   </StrictMode>,
-)
+);
